f7aa0eed3683316c59e53f5dee0112726649a089
bk <briansan24@gmail.com>
2014-12-11 03:26:44 -0500

    good for importing the model controller and the core classes

---
+++ py/model/__init__.py
@@ -0,0 +1,6 @@
+import Controller as BGModelController
+from Grade import Grade
+from Assignment import Assignment
+from Criteria import Criteria
+from Course import Course
+from Semester import Semester
